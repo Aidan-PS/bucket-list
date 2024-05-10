e9aec92cca102519d36678f024ddfd9944b23832
AidanPS <aidanps98@gmail.com>
2024-05-10 12:04:36 +0100

    Edit & delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,22 @@ import React, { useState } from 'react';
 import { hot } from "react-hot-loader/root";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BucketListItemProps, BucketListItem } from './components/BucketListItem';
+import { BucketListItem, BucketListItemProps } from './components/BucketListItem';
 
 const App = () => {
-    const [bucketListItems, setBucketListItems] = useState<BucketListItemProps[]>([
-        {
-            id: "123345345",
-            title: "Restaurant",
-            description: "Restaurant description",
-            location: "London",
-            completed: true,
-        },
-        {
-            id: "7563534",
-            title: "Museum",
-            description: "Museum description",
-            location: "Berlin",
-            completed: false
-        }
-    ]);
 
-    const [newItem, setNewItem] = useState<BucketListItemProps>({
-        id: '',
-        title: '',
-        description: '',
-        location: '',
-        completed: false
-    });
+    const deleteItem = (id: string): void => {
+        setBucketListItems(bucketListItems.filter(item => item.id !== id));
+    };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        const {name, value} = event.target;
-        setNewItem({
-            ...newItem,
-            [name]: value
-        })
-    }
+    const editItem = (id: string, data: BucketListItemProps): void => {
+        setBucketListItems(bucketListItems.map(item => {
+            if (item.id === id) {
+                return { ...item, ...data };
+            }
+            return item;
+        }));
+    };
 
     const addItem = (): void => {
         if (newItem.title.trim() !== '') {
@@ -44,57 +25,93 @@ const App = () => {
             setNewItem({
                 id: '',
                 title: '',
-                description: '',
+                category: '',
                 location: '',
                 completed: false
             });
         }
     };
 
-    return (
-        <>
-            <div className="d-flex justify-content-center align-items-center flex-column">
-                <h1 className="my-5">Bucket List</h1>
-                <div className='gap-1 p-4 rounded border'>
-                    <h5 className="fw-bold">New item</h5>
-                    <div className="d-flex flex-column gap-4">
-                        <div style={{width:'300px'}} className="d-flex align-items-center justify-content-between">
-                            Title
-                            <input 
-                                type="text"
-                                name="title"
-                                value={newItem.title}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div style={{width:'300px'}} className="d-flex align-items-center justify-content-between">
-                            Description
-                            <input 
-                                type="text"
-                                name="description"
-                                value={newItem.description}
-                                onChange={handleChange}
-                            />
-                        </div>
 
-                        <div style={{width:'300px'}} className="d-flex align-items-center justify-content-between">
-                            Location
-                            <input 
-                                type="text"
-                                name="location"
-                                value={newItem.location}
-                                onChange={handleChange}
-                            />
-                        </div>
-                  
+
+    const initialItems: BucketListItemProps[] = [
+        {
+            id: "123345345",
+            title: "Black Lock",
+            category: "Restaurant",
+            location: "London",
+            completed: true
+        },
+        {
+            id: "7563534",
+            title: "British Museum",
+            category: "Museum",
+            location: "Berlin",
+            completed: false
+        }
+    ];
+
+    const [bucketListItems, setBucketListItems] = useState<BucketListItemProps[]>(initialItems);
+
+    const [newItem, setNewItem] = useState<BucketListItemProps>({
+        id: '',
+        title: '',
+        category: '',
+        location: '',
+        completed: false
+    });
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target;
+        setNewItem({ ...newItem, [name]: value });
+    };
+
+
+    return (
+        <div className="container mt-5 d-flex justify-content-center align-items-center flex-column gap-4">
+            <div className="gap-3 p-4 rounded border">
+                <h5 className="fw-bold">New item</h5>
+                <div className="d-flex flex-column gap-4">
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Title
+                        <input 
+                            type="text"
+                            name="title"
+                            value={newItem.title}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Category
+                        <input 
+                            type="text"
+                            name="category"
+                            value={newItem.category}
+                            onChange={handleChange}
+                        />
+                    </label>
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Location
+                        <input 
+                            type="text"
+                            name="location"
+                            value={newItem.location}
+                            onChange={handleChange}
+                        />
+                    </label>
                 </div>
-                <button className="btn btn-success ms-auto mt-3" onClick={addItem}>Add</button>
-                </div>
-                <div className="d-flex flex-column gap-3 my-5" id="items-container">
-                    {bucketListItems.map(item => <BucketListItem key={item.id} {...item} />)}
-                </div>
+                <button className="btn btn-success mt-4" onClick={addItem}>Add</button>
             </div>
-        </>
+            <div className="d-flex flex-column gap-3 my-5" id="items-container">
+                {bucketListItems.map(item => (
+                    <BucketListItem 
+                        {...item}
+                        onDelete={deleteItem}
+                        onEdit={editItem}
+                    />
+                ))}
+            </div>
+        </div>
     );
 };
 
--- src/components/BucketListItem.tsx
+++ src/components/BucketListItem.tsx
@@ -3,13 +3,51 @@ import React, {useState} from 'react';
 export interface BucketListItemProps{
     id: string;
     title: string;
-    description: string;
+    category: string;
     location: string;
     completed: boolean;
 }
 
 
-export const BucketListItem = (props: BucketListItemProps): JSX.Element => {
+export const BucketListItem = (
+    props: BucketListItemProps
+    & {
+        onDelete: (id: string) => void,
+        onEdit: (id: string, data: BucketListItemProps) => void
+    }
+): JSX.Element => {
+
+
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [formData, setFormData] = useState<BucketListItemProps>({
+        id: props.id,
+        title: props.title,
+        category: props.category,
+        location: props.location,
+        completed: false
+    });
+
+    const handleEditClick = (): void => {
+        setEditMode(true);
+     
+    };
+
+    const handleCancelEdit= (): void => {
+        setEditMode(false);
+    };
+
+    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
+    const handleEditSubmit = (event: React.FormEvent): void => {
+        event.preventDefault();
+        props.onEdit(props.id, formData);
+        setEditMode(false);
+
+    }
+
 
     const [isChecked, setIsChecked] = useState<boolean>(props.completed);
 
@@ -17,20 +55,73 @@ export const BucketListItem = (props: BucketListItemProps): JSX.Element => {
         setIsChecked(!isChecked);
     }
 
+
     return (
-        <div>
-            <div data-item-id={props.id} className="fw-bold">{props.title}</div>
-                <div>{props.description}</div>
-                <div>{props.location}</div>
-                <label>
-                    Completed:
-                    <input
-                        type="checkbox"
-                        checked={isChecked}
-                        className = 'ms-2'
-                        onChange={handleCheckboxChange}
-                    />
-                </label>
+        <div data-item-id={props.id} className="bg-light p-3 shadow-sm rounded d-flex flex-column gap-2" style={{width:"500px"}}>
+            {!editMode ? (
+                <>
+                    <div className="fw-bold">{props.title}</div>
+                    <div>{props.category}</div>
+                    <div>{props.location}</div>
+                    <label>
+                        Completed:
+                        <input
+                            type="checkbox"
+                            checked={props.completed}
+                            className="ms-2"
+                            onChange={handleCheckboxChange}
+                        />
+                    </label>
+                    <div className="d-flex align-items-center gap-3">
+                        <button 
+                            className="btn btn-info"
+                            onClick={handleEditClick}
+                        >
+                            Edit
+                        </button>
+                        <button 
+                            className="btn btn-danger me-auto"
+                            onClick={() => props.onDelete(props.id)}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                </>
+            ) : (
+                <form onSubmit={handleEditSubmit} className="d-flex flex-column gap-3">
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Title
+                        <input 
+                            type="text"
+                            name="title"
+                            value={formData.title}
+                            onChange={handleFormChange}
+                        />
+                    </label>
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Category
+                        <input 
+                            type="text"
+                            name="category"
+                            value={formData.category}
+                            onChange={handleFormChange}
+                        />
+                    </label>
+                    <label style={{width:"300px"}} className='d-flex justify-content-between align-items-center'>
+                        Location
+                        <input 
+                            type="text"
+                            name="location"
+                            value={formData.location}
+                            onChange={handleFormChange}
+                        />
+                    </label>
+                    <div className="d-flex align-items-center gap-3">
+                        <button type="submit" className='btn btn-success'>Save</button>
+                        <button type="button" className='btn' onClick={handleCancelEdit}>Cancel</button>
+                    </div>
+                </form>
+            )}
         </div> 
-    )
+    );
 }
