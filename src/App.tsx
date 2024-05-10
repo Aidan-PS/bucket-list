d8e5deea53d6bd6a3f5775105738e2a2b69f6c31
AidanPS <aidanps98@gmail.com>
2024-05-10 08:43:21 +0100

    Adjusted App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,16 @@ import { hot } from "react-hot-loader/root";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
-      <>
+        <>
         <div className="d-flex justify-content-center align-items-center">
             <h1>Bucket List</h1>
         </div>
       </>
-    );
-  }
+    )
 }
+
+
 
 export default hot(App);
